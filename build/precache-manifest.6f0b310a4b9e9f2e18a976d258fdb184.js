81a5aa710e070f8302641d0532f9deab6c2235c6
racheliwaner <racheliwaner@gmail.com>
2020-06-16 22:26:22 +0300

    jjjjj

---
+++ build/precache-manifest.6f0b310a4b9e9f2e18a976d258fdb184.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06fe931cf45726f653fa895048dc285",
+    "url": "/breastApp/index.html"
+  },
+  {
+    "revision": "71325b2adff7c4ffcecb",
+    "url": "/breastApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f673c6b6b075115d55f",
+    "url": "/breastApp/static/js/2.43229570.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breastApp/static/js/2.43229570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71325b2adff7c4ffcecb",
+    "url": "/breastApp/static/js/main.2933c249.chunk.js"
+  },
+  {
+    "revision": "7455de84471b66d7199f",
+    "url": "/breastApp/static/js/runtime-main.cfc6b875.js"
+  },
+  {
+    "revision": "d966e200503a3403f9508084dc4aa02c",
+    "url": "/breastApp/static/media/IMG1.d966e200.jpg"
+  },
+  {
+    "revision": "20d4d512e00a0cacf6a47a2b119e3812",
+    "url": "/breastApp/static/media/IMG4.20d4d512.jpg"
+  },
+  {
+    "revision": "0bbaa9b82035ff0ace866b9e51e9deaa",
+    "url": "/breastApp/static/media/IMG5.0bbaa9b8.jpg"
+  },
+  {
+    "revision": "c9bf04627519c8f879eb3bc47cf3fed7",
+    "url": "/breastApp/static/media/IMG6.c9bf0462.jpg"
+  },
+  {
+    "revision": "625d1d78b823d48673dbbd351d5040d2",
+    "url": "/breastApp/static/media/img10.625d1d78.jpg"
+  },
+  {
+    "revision": "c8fdcc4c69eb9d0e7c7c99d8b949133f",
+    "url": "/breastApp/static/media/img11.c8fdcc4c.jpg"
+  },
+  {
+    "revision": "635f1948f46b04e0dc52a70e0dddf849",
+    "url": "/breastApp/static/media/img12.635f1948.jpg"
+  },
+  {
+    "revision": "033caada04cef4403c51f9dd9bedc523",
+    "url": "/breastApp/static/media/img13.033caada.jpg"
+  },
+  {
+    "revision": "b36f0d85138e3311e2355424bd505267",
+    "url": "/breastApp/static/media/img14.b36f0d85.jpg"
+  },
+  {
+    "revision": "10265d858af4065e4957447e3e2754f6",
+    "url": "/breastApp/static/media/img15.10265d85.jpg"
+  },
+  {
+    "revision": "90e7a2b68486bbad081408ec60d75f63",
+    "url": "/breastApp/static/media/img16.90e7a2b6.jpg"
+  },
+  {
+    "revision": "acf738436244afc2b696b9e182a42979",
+    "url": "/breastApp/static/media/img7.acf73843.jpg"
+  },
+  {
+    "revision": "06c094c14002d8ac0e824e66a26f127c",
+    "url": "/breastApp/static/media/img8.06c094c1.jpg"
+  },
+  {
+    "revision": "03386b6f40bfabc8a65d02b3d0696c46",
+    "url": "/breastApp/static/media/img9.03386b6f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/breastApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "331ec5aedf1d071dab613edc9e851947",
+    "url": "/breastApp/static/media/main_pic.331ec5ae.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 //import './App.css';
 import ReactDOM from 'react-dom';
 //import Button from 'react-bootstrap/Button'
-import Background from 'C:/Users/DELL/racheliwaner.github.io/src/main_pic.jpg'; 
+import Background from 'C:/Users/DELL/breastApp/src/main_pic.jpg'; 
 
     
   class App extends Component{
@@ -12,20 +12,20 @@ import Background from 'C:/Users/DELL/racheliwaner.github.io/src/main_pic.jpg';
       this.onClickForward = this.onClickForward.bind(this)
       this.onClickBack = this.onClickBack.bind(this)
      
-     const IMG1 = require('C:/Users/DELL/racheliwaner.github.io/src/IMG5.jpg');
-     const IMG2 = require('C:/Users/DELL/racheliwaner.github.io/src/IMG6.jpg');
-     const IMG3 = require('C:/Users/DELL/racheliwaner.github.io/src/IMG1.jpg');
-     const IMG4 = require('C:/Users/DELL/racheliwaner.github.io/src/IMG4.jpg');
-     const img7 = require('C:/Users/DELL/racheliwaner.github.io/src/img7.jpg');
-     const img8 = require('C:/Users/DELL/racheliwaner.github.io/src/img8.jpg');
-     const img9 = require('C:/Users/DELL/racheliwaner.github.io/src/img9.jpg');
-     const img10 = require('C:/Users/DELL/racheliwaner.github.io/src/img10.jpg');
-     const img11 = require('C:/Users/DELL/racheliwaner.github.io/src/img11.jpg');
-     const img12 = require('C:/Users/DELL/racheliwaner.github.io/src/img12.jpg');
-     const img13 = require('C:/Users/DELL/racheliwaner.github.io/src/img13.jpg');
-     const img14 = require('C:/Users/DELL/racheliwaner.github.io/src/img14.jpg');
-     const img15 = require('C:/Users/DELL/racheliwaner.github.io/src/img15.jpg');
-     const img16 = require('C:/Users/DELL/racheliwaner.github.io/src/img16.jpg');
+     const IMG1 = require('C:/Users/DELL/breastApp/src/IMG5.jpg');
+     const IMG2 = require('C:/Users/DELL/breastApp/src/IMG6.jpg');
+     const IMG3 = require('C:/Users/DELL/breastApp/src/IMG1.jpg');
+     const IMG4 = require('C:/Users/DELL/breastApp/src/IMG4.jpg');
+     const img7 = require('C:/Users/DELL/breastApp/src/img7.jpg');
+     const img8 = require('C:/Users/DELL/breastApp/src/img8.jpg');
+     const img9 = require('C:/Users/DELL/breastApp/src/img9.jpg');
+     const img10 = require('C:/Users/DELL/breastApp/src/img10.jpg');
+     const img11 = require('C:/Users/DELL/breastApp/src/img11.jpg');
+     const img12 = require('C:/Users/DELL/breastApp/src/img12.jpg');
+     const img13 = require('C:/Users/DELL/breastApp/src/img13.jpg');
+     const img14 = require('C:/Users/DELL/breastApp/src/img14.jpg');
+     const img15 = require('C:/Users/DELL/breastApp/src/img15.jpg');
+     const img16 = require('C:/Users/DELL/breastApp/src/img16.jpg');
 
      this.state = {index: 0, imgList:[IMG1,IMG2,IMG3,IMG4,img7,
       img8,img9,img10,img11,img12,img13,img14, img15, img16], string: 'אישה בת 52 שהגיעה לבדיקת סקר, כתבו שהממוגרפיה רקמה סמיכה ולכן המליצו על אולטרסאונד באולולטרסאונד מצאתי גוש בכל שד '}
@@ -61,7 +61,7 @@ import Background from 'C:/Users/DELL/racheliwaner.github.io/src/main_pic.jpg';
       const mystyle = {color: "black",fontSize: 30,paddingBottom: 10, display: "flex",
                        justifyContent: "center", alignItems: "center", fontFamily: "Open Sans"};
       const button = { marginLeft: '17%',borderRadius: 8, height: 400, width: 400 };
-      const backImage = {backgroundImage: `url(${require('C:/Users/DELL/racheliwaner.github.io/src/main_pic.jpg')})` ,
+      const backImage = {backgroundImage: `url(${require('C:/Users/DELL/breastApp/src/main_pic.jpg')})` ,
         backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"}
       return (
         <div>
