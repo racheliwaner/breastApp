b18ffd60d7cb2be7bfb5b8690c3e3d9cb1f37b8b
racheliwaner <racheliwaner@gmail.com>
2020-06-20 22:05:18 +0300

    Updates

---
+++ precache-manifest.525915f7d18d3131fa1e5259cdea2d71.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025d45e3e6e205c0d451c5db309e0602",
+    "url": "/breastApp/index.html"
+  },
+  {
+    "revision": "c9c21703953eb2e90a08",
+    "url": "/breastApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cba4a0466b1dc2aa2eb7",
+    "url": "/breastApp/static/css/main.e85e0c64.chunk.css"
+  },
+  {
+    "revision": "c9c21703953eb2e90a08",
+    "url": "/breastApp/static/js/2.71f21470.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/breastApp/static/js/2.71f21470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba4a0466b1dc2aa2eb7",
+    "url": "/breastApp/static/js/main.2d0446ed.chunk.js"
+  },
+  {
+    "revision": "7455de84471b66d7199f",
+    "url": "/breastApp/static/js/runtime-main.cfc6b875.js"
+  },
+  {
+    "revision": "895e103617ce53cd4343e062bcf72cd5",
+    "url": "/breastApp/static/media/IMG1.895e1036.jpg"
+  },
+  {
+    "revision": "3da966395c6b89e979df936fc6384d1b",
+    "url": "/breastApp/static/media/IMG4.3da96639.jpg"
+  },
+  {
+    "revision": "1b1b217cefa56110e67f23d79a3ba51e",
+    "url": "/breastApp/static/media/IMG5.1b1b217c.jpg"
+  },
+  {
+    "revision": "1d0ea3ce1f6c0a78d4ccab0cc97d8256",
+    "url": "/breastApp/static/media/IMG6.1d0ea3ce.jpg"
+  },
+  {
+    "revision": "8089d3547fd9745b8a4c0cec2580d550",
+    "url": "/breastApp/static/media/KeterYG-BoldOblique.8089d354.sfd"
+  },
+  {
+    "revision": "8374d2930429485ee1777a063a1f5d13",
+    "url": "/breastApp/static/media/img10.8374d293.jpg"
+  },
+  {
+    "revision": "7ca20e7c28845b007b526bf1d45edd3a",
+    "url": "/breastApp/static/media/img14.7ca20e7c.jpg"
+  },
+  {
+    "revision": "084e8130b87f6100aaf95394a9c22d10",
+    "url": "/breastApp/static/media/img15.084e8130.jpg"
+  },
+  {
+    "revision": "01a039f0de5c9597e99575ac16257bdd",
+    "url": "/breastApp/static/media/img16.01a039f0.jpg"
+  },
+  {
+    "revision": "0819ffcd13cd58d902f5dfc39df75153",
+    "url": "/breastApp/static/media/img7.0819ffcd.jpg"
+  },
+  {
+    "revision": "91819b42eb845dfc25544d668a0055fb",
+    "url": "/breastApp/static/media/img8.91819b42.jpg"
+  },
+  {
+    "revision": "42ada53a9288f5fd364e0752dce741ed",
+    "url": "/breastApp/static/media/img9.42ada53a.jpg"
+  },
+  {
+    "revision": "82bc58269d0ffc8256edf0ec88d20907",
+    "url": "/breastApp/static/media/main_page2.82bc5826.jpg"
+  },
+  {
+    "revision": "91bfe7f50162764469f1c082c1106849",
+    "url": "/breastApp/static/media/shuneet3-bold.91bfe7f5.otf"
+  }
+]);
